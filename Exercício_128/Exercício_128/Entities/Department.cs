0898dd94a29b463461316f878df5bf7d11cfe05a
Hugo dos Santos <victorhugo0856@gmail.com>
2022-10-03 12:25:37 -0300

    Exercicio resolvido Enumerações e composição

---
+++ Exercício_128/Exercício_128/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercício_128.Entities
+{
+    internal class Department
+    {
+        public String Name { get; set; }
+
+       
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Exercício_128/Exercício_128/Program.cs
@@ -0,0 +1,51 @@
+﻿using Exercício_128.Entities;
+using Exercício_128.Entities.Enums;
+using System.Globalization;
+using System;
+
+namespace course
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Juniro/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double ValuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, ValuePerHour, hours);
+                worker.AddContract(contract);
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
