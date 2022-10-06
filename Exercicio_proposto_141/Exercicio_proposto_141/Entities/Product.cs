bdf56783befd551df4782f1926a26c76e6024ab9
Hugo dos Santos <victorhugo0856@gmail.com>
2022-10-06 12:21:48 -0300

    Exercicio proposto herança e polimorfismo 142

---
+++ Exercicio_proposto_141/Exercicio_proposto_141/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercicio_proposto_141.Entities
+{
+    internal class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name
+                + " $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
+
---
+++ Exercicio_proposto_141/Exercicio_proposto_141/Entities/UsedProduct.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Exercicio_proposto_141.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+
+        public UsedProduct(string name, double price, DateTime manuFactureDate) : base(name, price)
+        {
+            ManufactureDate = manuFactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "               
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
