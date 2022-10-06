ff98ca73fd801573316fc5be8770439a41fc8b6f
Hugo dos Santos <victorhugo0856@gmail.com>
2022-10-06 09:35:04 -0300

    Exercicio Resolvido 140

---
+++ Exercicio_Resolvido_140/Exercicio_Resolvido_140/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Exercicio_Resolvido_140.Entities;
+namespace course
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{1} data: ");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double ValuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additonalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, ValuePerHour, additonalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, ValuePerHour));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PAYMENTS: ");
+                foreach(Employee emp in list)
+                {
+                    Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
